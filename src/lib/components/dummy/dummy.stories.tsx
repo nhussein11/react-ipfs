55fb61620c53713727d27cf7195ef14862f88d24
nhussein11 <nicolashussein14@gmail.com>
2023-07-29 17:39:41 -0300

    fix: button test

--- src/lib/components/dummy/dummy.stories.tsx
+++ src/lib/components/dummy/dummy.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Dummy } from '.'
+import { Dummy, DummyProps } from '.'
 import { Meta } from '@storybook/react'
 import { StoryFn } from '@storybook/react'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Dummy> = {
-  title: 'Atoms/Button',
+  title: 'Ipfs/Button',
   component: Dummy,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -15,10 +15,10 @@ const meta: Meta<typeof Dummy> = {
 export default meta
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: StoryFn<typeof Dummy> = () => <Dummy />
+const Template: StoryFn<typeof Dummy> = (args: DummyProps) => <Dummy {...args} />
 
 export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  label: 'Dummy',
+  label: 'Test',
 }
--- src/lib/components/dummy/index.tsx
+++ src/lib/components/dummy/index.tsx
@@ -1,3 +1,7 @@
-export const Dummy = () => {
-  return <div>Dummy</div>
+export interface DummyProps {
+  label: string
+}
+
+export const Dummy = ({ label }: DummyProps): JSX.Element => {
+  return <button className="border border-black px-5 py-2 rounded-xl">{label}</button>
 }
