62496d1c7be6222501cd1479da49c901df9ae71a
nhussein11 <nicolashussein14@gmail.com>
2023-07-30 19:24:25 -0300

    fix: update image story adding custom gateway story

---
+++ src/lib/components/image/image.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StoryFn, Meta } from '@storybook/react'
+import { ImageIPFS, IpfsImageProps } from '.'
+
+const meta: Meta<typeof ImageIPFS> = {
+  title: 'IpfsImage',
+  component: ImageIPFS,
+  argTypes: {
+    hash: {
+      default: '',
+    },
+  },
+}
+
+export default meta
+
+const Template: StoryFn<typeof ImageIPFS> = (args: IpfsImageProps) => <ImageIPFS {...args} />
+
+export const DefaultImage = Template.bind({})
+
+DefaultImage.args = {
+  hash: 'QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi',
+}
+
+export const SpecificGatewayImage = Template.bind({})
+
+SpecificGatewayImage.args = {
+  hash: 'QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi',
+  gateaway: 'gateway.moralisipfs.com',
+}
