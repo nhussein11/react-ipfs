080a0a61ff6b9cab6fdb3f3e4d6a42e7672668f4
nhussein11 <nicolashussein14@gmail.com>
2023-07-30 20:26:41 -0300

    add: unit test to image ipfs component

---
+++ src/lib/components/image/image.test.tsx
@@ -0,0 +1,60 @@
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { ImageIPFS } from './index'
+
+const MOCK_HASH = 'QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi'
+
+describe('IPFSImage', () => {
+  it('should render the IPFSImage component', () => {
+    render(<ImageIPFS hash={MOCK_HASH} />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+  })
+
+  it('should render the IPFSImage component with a specific gateway', () => {
+    render(<ImageIPFS hash={MOCK_HASH} gateway="gateway.moralisipfs.com" />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image).toHaveProperty(
+      'src',
+      'https://gateway.moralisipfs.com/ipfs/QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi',
+    )
+  })
+
+  it('should render the IPFSImage component with default size', () => {
+    render(<ImageIPFS hash={MOCK_HASH} gateway="gateway.moralisipfs.com" width="100" height="100" />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image).toHaveProperty('width', 100)
+    expect(image).toHaveProperty('height', 100)
+  })
+
+  it('should render the IPFSImage component with custom size', () => {
+    render(<ImageIPFS hash={MOCK_HASH} width={200} height={200} />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image).toHaveProperty('width', 200)
+    expect(image).toHaveProperty('height', 200)
+  })
+
+  it('should render the IPFSImage component with custom alt', () => {
+    render(<ImageIPFS hash={MOCK_HASH} alt="custom alt" />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image).toHaveProperty('alt', 'custom alt')
+  })
+
+  it('should render the IPFSImage component with lazy loading', () => {
+    render(<ImageIPFS hash={MOCK_HASH} lazy={true} />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image.getAttribute('loading')).toBe('lazy')
+  })
+
+  it('should render the IPFSImage component with eager loading', () => {
+    render(<ImageIPFS hash={MOCK_HASH} />)
+    const image = screen.getByRole('img')
+    expect(image).toBeDefined()
+    expect(image.getAttribute('loading')).toBe('eager')
+  })
+})
