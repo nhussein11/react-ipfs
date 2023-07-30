66118742c143196007b57e7a84d972d31a562a7b
nhussein11 <nicolashussein14@gmail.com>
2023-07-30 20:32:52 -0300

    fix: image component with styles and classnames

--- src/lib/components/image/index.tsx
+++ src/lib/components/image/index.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from 'react'
+import { CSSProperties, HTMLAttributes, HTMLProps } from 'react'
 import { getIpfsURL } from '../../utils/ipfs'
 
 export interface IpfsImageProps extends HTMLAttributes<HTMLImageElement> {
@@ -8,14 +8,37 @@ export interface IpfsImageProps extends HTMLAttributes<HTMLImageElement> {
   height?: number
   alt?: string
   lazy?: boolean
+  style?: CSSProperties
+  className?: HTMLProps<HTMLElement>['className']
 }
 
-export const ImageIPFS = ({ hash, gateway, width, height, alt, lazy, ...props }: IpfsImageProps): JSX.Element => {
+export const ImageIPFS = ({
+  hash,
+  gateway,
+  width,
+  height,
+  alt,
+  lazy,
+  style,
+  className,
+  ...props
+}: IpfsImageProps): JSX.Element => {
   const imgSrc = getIpfsURL(gateway, hash)
   const imgWidth = width ?? 100
   const imgHeight = height ?? 100
   const imgAlt = alt ?? 'IPFS Image'
   const imgLoading = lazy === true ? 'lazy' : 'eager'
 
-  return <img src={imgSrc} alt={imgAlt} width={imgWidth} height={imgHeight} loading={imgLoading} {...props} />
+  return (
+    <img
+      src={imgSrc}
+      alt={imgAlt}
+      width={imgWidth}
+      height={imgHeight}
+      style={style}
+      className={className}
+      loading={imgLoading}
+      {...props}
+    />
+  )
 }
