90b50685019d7002d39318d93aca9292d64bac0c
nhussein11 <nicolashussein14@gmail.com>
2023-07-31 15:44:55 -0300

    fix: remove dummy component

--- src/lib/components/dummy/index.tsx
+++
@@ -1,7 +0,0 @@
-export interface DummyProps {
-  label: string
-}
-
-export const Dummy = ({ label }: DummyProps): JSX.Element => {
-  return <button className="border border-black px-5 py-2 rounded-xl">{label}</button>
-}
