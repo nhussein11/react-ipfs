a99d285dfd4aec5690446607d858f82c1e3371d1
nhussein11 <nicolashussein14@gmail.com>
2023-08-06 16:56:40 -0300

    fix: adding more unit test to video component

--- src/lib/components/video/video.test.tsx
+++ src/lib/components/video/video.test.tsx
@@ -49,4 +49,62 @@ describe('IPFSVideo', () => {
     const loading = screen.getByText('Loading...')
     expect(loading).toBeDefined()
   })
+  it('should render the IPFSVideo component with a gateway and a thumbnail and a loading component and muted', () => {
+    render(
+      <VideoIPFS
+        hash="Qmc2oeTErghqoTB3N9j8te8QEfGRPTfTNH77aXARiMeuDJ"
+        gateway="https://ipfs.io/ipfs/"
+        thumbnail="https://ipfs.io/ipfs/QmZ9y1Vd7X9s8XG4Zz1Xh7k2Lj6Z5oQ6c5sJgU7h6LQ3bC"
+        loadingComponent={<div>Loading...</div>}
+        muted
+      />,
+    )
+    const video = document.querySelector('video')
+    expect(video).toBeDefined()
+    const loading = screen.getByText('Loading...')
+    expect(loading).toBeDefined()
+    const muted = video?.getAttribute('muted')
+    expect(muted).toBeDefined()
+  })
+  it('should render the IPFSVideo component with a gateway and a thumbnail and a loading component and muted and autoplay', () => {
+    render(
+      <VideoIPFS
+        hash="Qmc2oeTErghqoTB3N9j8te8QEfGRPTfTNH77aXARiMeuDJ"
+        gateway="https://ipfs.io/ipfs/"
+        thumbnail="https://ipfs.io/ipfs/QmZ9y1Vd7X9s8XG4Zz1Xh7k2Lj6Z5oQ6c5sJgU7h6LQ3bC"
+        loadingComponent={<div>Loading...</div>}
+        muted
+      />,
+    )
+    const video = document.querySelector('video')
+    expect(video).toBeDefined()
+    const loading = screen.getByText('Loading...')
+    expect(loading).toBeDefined()
+    const muted = video?.getAttribute('muted')
+    expect(muted).toBeDefined()
+    const autoplay = video?.getAttribute('autoplay')
+    expect(autoplay).toBeDefined()
+  })
+  it('should render the IPFSVideo component with a gateway and a thumbnail and a loading component and muted and autoplay and loop', () => {
+    render(
+      <VideoIPFS
+        hash="Qmc2oeTErghqoTB3N9j8te8QEfGRPTfTNH77aXARiMeuDJ"
+        gateway="https://ipfs.io/ipfs/"
+        thumbnail="https://ipfs.io/ipfs/QmZ9y1Vd7X9s8XG4Zz1Xh7k2Lj6Z5oQ6c5sJgU7h6LQ3bC"
+        loadingComponent={<div>Loading...</div>}
+        muted
+        loop
+      />,
+    )
+    const video = document.querySelector('video')
+    expect(video).toBeDefined()
+    const loading = screen.getByText('Loading...')
+    expect(loading).toBeDefined()
+    const muted = video?.getAttribute('muted')
+    expect(muted).toBeDefined()
+    const autoplay = video?.getAttribute('autoplay')
+    expect(autoplay).toBeDefined()
+    const loop = video?.getAttribute('loop')
+    expect(loop).toBeDefined()
+  })
 })
