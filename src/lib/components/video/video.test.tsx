1f53dedbca452714a1381d83673611e702c668f9
nhussein11 <nicolashussein14@gmail.com>
2023-08-06 17:49:53 -0300

    fix: volume video unit test

--- src/lib/components/video/video.test.tsx
+++ src/lib/components/video/video.test.tsx
@@ -107,4 +107,46 @@ describe('IPFSVideo', () => {
     const loop = video?.getAttribute('loop')
     expect(loop).toBeDefined()
   })
+  it('should render the IPFSVideo component with a gateway and a thumbnail and a loading component and muted and autoplay and loop and controls', () => {
+    render(
+      <VideoIPFS
+        hash="Qmc2oeTErghqoTB3N9j8te8QEfGRPTfTNH77aXARiMeuDJ"
+        gateway="https://ipfs.io/ipfs/"
+        thumbnail="https://ipfs.io/ipfs/QmZ9y1Vd7X9s8XG4Zz1Xh7k2Lj6Z5oQ6c5sJgU7h6LQ3bC"
+        loadingComponent={<div>Loading...</div>}
+        muted
+        loop
+        controls
+      />,
+    )
+    const video = document.querySelector('video')
+    expect(video).toBeDefined()
+    const loading = screen.getByText('Loading...')
+    expect(loading).toBeDefined()
+    const muted = video?.getAttribute('muted')
+    expect(muted).toBeDefined()
+    const autoplay = video?.getAttribute('autoplay')
+    expect(autoplay).toBeDefined()
+    const loop = video?.getAttribute('loop')
+    expect(loop).toBeDefined()
+    const controls = video?.getAttribute('controls')
+    expect(controls).toBeDefined()
+  })
+  it('should render the IPFSVideo component with specific volume', () => {
+    render(
+      <VideoIPFS
+        hash="Qmc2oeTErghqoTB3N9j8te8QEfGRPTfTNH77aXARiMeuDJ"
+        gateway="https://ipfs.io/ipfs/"
+        thumbnail="https://ipfs.io/ipfs/QmZ9y1Vd7X9s8XG4Zz1Xh7k2Lj6Z5oQ6c5sJgU7h6LQ3bC"
+        loadingComponent={<div>Loading...</div>}
+        muted
+        loop
+        controls
+        volume={0.5}
+      />,
+    )
+    const video = document.querySelector('video')
+    expect(video).toBeDefined()
+    expect(video.volume).toBe(0.5)
+  })
 })
