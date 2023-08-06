57f957baeb7a76b0484859f79c4a0d3efa99f1ef
nhussein11 <nicolashussein14@gmail.com>
2023-08-06 16:51:54 -0300

    fix: video component

--- src/lib/components/video/index.tsx
+++ src/lib/components/video/index.tsx
@@ -1,25 +1,52 @@
-import { useState, type HTMLAttributes, useLayoutEffect } from 'react'
+import { useLayoutEffect, useState, type HTMLAttributes, type ReactNode } from 'react'
 import { getIpfsURL } from '../../utils/ipfs'
+
+const DefaultLoadingComponent = (): JSX.Element => {
+  return <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">Loading...</div>
+}
 
 export interface IpfsVideoProps extends HTMLAttributes<HTMLVideoElement> {
   hash: string
   gateway?: string
   muted?: boolean
+  loop?: boolean
+  loadingComponent?: ReactNode
+  thumbnail?: string
 }
 
-export const VideoIPFS = ({ hash, gateway, muted = true }: IpfsVideoProps): JSX.Element => {
+export const VideoIPFS = ({
+  hash,
+  gateway,
+  muted = true,
+  loop = true,
+  loadingComponent = <DefaultLoadingComponent />,
+  thumbnail,
+  ...rest
+}: IpfsVideoProps): JSX.Element => {
   const [videoUrl, setVideoUrl] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(true)
 
   useLayoutEffect(() => {
     const cleanedVideoURL = getIpfsURL(gateway, hash)
     setVideoUrl(cleanedVideoURL)
   }, [hash, gateway])
 
+  const handleLoadedData = (): void => {
+    setLoading(false)
+  }
+
   return (
     <>
-      <video autoPlay muted={muted} playsInline>
-        <source src={videoUrl} type="video/mp4" />
-      </video>
+      <div className="relative">
+        {loading && loadingComponent}
+        {thumbnail !== undefined && videoUrl === undefined && (
+          <img src={thumbnail} className="absolute top-0 left-0 w-full h-full object-cover" />
+        )}
+        <video autoPlay muted={muted} loop={loop} playsInline {...rest} onLoadedData={handleLoadedData}>
+          <source src={videoUrl} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </>
   )
 }
